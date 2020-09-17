6235d0c7d5713759dc3187c692e6b36505069c2c
Maddi Laizure <2331@holbertonschool.com>
2020-09-17 19:33:52 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+	char new = '\n';
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	{
+		if (ch != 'q' && ch != 'e')
+		putchar(ch);
+	}
+
+	putchar(new);
+
+	return (0);
+}
