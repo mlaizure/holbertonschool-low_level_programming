6df1652149454982415c734fbd7ec0709c7b49f0
Maddi Laizure <2331@holbertonschool.com>
2020-09-22 03:50:19 +0000

    Fixed typos on 101 and 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-	long int f1 = 1, f2 = 1, next = 0, sum= 0;
+	long int f1 = 1, f2 = 1, next = 0, sum = 0;
 
 	while (next < 4000000)
 	{
 		if (next % 2 == 0)
-       			sum += next;
-       	        next = f1 + f2;
-       		f1 = f2;
-       		f2 = next;
+			sum += next;
+		next = f1 + f2;
+		f1 = f2;
+		f2 = next;
 	}
 	printf("%ld\n", sum);
 	return (0);
