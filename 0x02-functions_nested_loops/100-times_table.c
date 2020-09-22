db8210d637e79e48a6326ffe6902da2a8206bdae
Maddi Laizure <2331@holbertonschool.com>
2020-09-22 05:42:18 +0000

    Fixed output for 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -47,7 +47,7 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n <= 15 && n >= 0)
+	if (n <= 15 && n > 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -65,5 +65,10 @@ void print_times_table(int n)
 					_putchar(k + '0');
 			}
 		}
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
 	}
 }
