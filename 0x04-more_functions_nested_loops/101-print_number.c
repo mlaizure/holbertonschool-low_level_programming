5ed0b79e5cfc1083ada7c4fc29b8b65239f24a5d
Maddi Laizure <2331@holbertonschool.com>
2020-09-24 23:09:02 +0000

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer
+ * @n: n is the number to print
+ *
+ * Return: none
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
