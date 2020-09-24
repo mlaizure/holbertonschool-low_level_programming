97166a846a8a38f36582ef76699dee3243c1bc47
Maddi Laizure <2331@holbertonschool.com>
2020-09-24 20:47:53 +0000

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triange
+ * @size: size of triangle
+ *
+ * Return: none
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; ++i)
+		{
+			for (j = size - 1; j > i; --j)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; ++k)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
