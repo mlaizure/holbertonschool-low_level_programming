02dc0f9da2358819f8071db04f2d2eccf9bc1e3d
Maddi Laizure <2331@holbertonschool.com>
2020-09-24 21:51:04 +0000

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor of 612852475143
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int i = 612852475143, div = 2, maxP;
+
+	while (i != 0)
+	{
+		if (i % div != 0)
+			div = div + 1;
+		else
+		{
+			maxP = i;
+			i /= div;
+			if (i == 1)
+			{
+				printf("%ld\n", maxP);
+				break;
+			}
+		}
+	}
+	return(0);
+}
