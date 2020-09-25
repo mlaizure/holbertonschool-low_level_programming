2d0c7e57637a62570c8e8c6973ad0b3509d1c05d
Maddi Laizure <2331@holbertonschool.com>
2020-09-25 00:28:21 +0000

    Fixed int overflow

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,14 @@
 
 void print_number(int n)
 {
-	if (n < 0)
-	{
+	if (n / 10 == 0 && n < 0)
 		_putchar('-');
-		n = -n;
-	}
 
 	if (n / 10)
 		print_number(n / 10);
 
-	_putchar(n % 10 + '0');
+	if (n % 10 < 0)
+		_putchar(-(n % 10) + '0');
+	else
+		_putchar(n % 10 + '0');
 }
