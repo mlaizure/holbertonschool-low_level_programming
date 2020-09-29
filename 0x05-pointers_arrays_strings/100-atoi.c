555b6cb0504df895aaf0166a71a91240882dda36
Maddi Laizure <2331@holbertonschool.com>
2020-09-29 17:25:38 +0000

    Added null logic

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,14 +9,14 @@ int _atoi(char *s)
 {
 	int i = 0, j = 0, sign = 1;
 
-	while (s[i] < '0' || s[i] > '9')
+	while ((s[i] < '0' || s[i] > '9') && s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		i++;
 	}
 
-	while (s[i] >= '0' && s[i] <= '9')
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
 	{
 		j = j * 10 + (s[i] - '0');
 		i++;
