1c223fa59d6d81335ba0ab0c617be46e95012e96
Maddi Laizure <2331@holbertonschool.com>
2020-09-29 17:07:31 +0000

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+/**
+ * _atoi - converts a string to an int
+ * @s: pointer to a string
+ *
+ * Return: int from string (0 if string contains no ints)
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, j = 0, sign = 1;
+
+	while (s[i] < '0' || s[i] > '9')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		j = j * 10 + s[i];
+		i++;
+	}
+
+	return (j);
+}
