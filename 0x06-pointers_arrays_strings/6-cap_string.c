f172d51ab722988490c54a31ffc74261b52b451a
Maddi Laizure <2331@holbertonschool.com>
2020-10-01 21:06:07 +0000

    Added capitalization for first letter

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -28,8 +28,12 @@ char *cap_string(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; ++i)
+	{
 		if (isItChar(s[i], " \t\n,;.!?\"(){}"))
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				s[i + 1] = s[i + 1] - 32;
+	if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 32;
+	}
 	return (s);
 }
