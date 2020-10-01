48a81d846e67dee365bcfb65be1232b6ff999894
Maddi Laizure <2331@holbertonschool.com>
2020-10-01 21:56:40 +0000

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,19 @@
+/**
+ * leet - encodes a string into 1337
+ * @s - string to be encoded
+ *
+ * Return: encoded string.
+ */
+
+char *leet(char *s)
+{
+	unsigned int i, j;
+	char arrl[] = {'a', 'e', 'o', 't', 'l'};
+	char arrn[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; ++i)
+		for (j = 0; j < sizeof(arrl); ++j)
+			if (arrl[j] == s[i] || arrl[j] - 32 == s[i])
+				s[i] = arrn[j];
+	return (s);
+}
