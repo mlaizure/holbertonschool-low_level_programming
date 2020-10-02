659bf242ce3ceba371c76d92f227e3bb0adc8daf
Maddi Laizure <2331@holbertonschool.com>
2020-10-02 17:50:34 +0000

    Track the main files on a separate branch

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_times_table(1);
+	_putchar('\n');
+	print_times_table(16);
+	_putchar('\n');
+	print_times_table(15);
+	_putchar('\n');
+	print_times_table(0);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_number(-2147483648);
+	_putchar('\n');
+	return (0);
+}
