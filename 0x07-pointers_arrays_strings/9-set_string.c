7a693d371c79a29a712ecd88f7df0de88ea103a0
Maddi Laizure <2331@holbertonschool.com>
2020-10-05 23:41:11 +0000

    Task 8

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to a pointer
+ * @to: pointer
+ *
+ * Return: none.
+ */
+
+void set_string(char **s, char *to)
+{
+    *s = to;
+}
