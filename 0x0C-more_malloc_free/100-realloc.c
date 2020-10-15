70f62e14026a9bc15dbc68a3d142496ba10ae4e5
Maddi Laizure <2331@holbertonschool.com>
2020-10-15 23:14:13 +0000

    Testing against checker

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,9 +23,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr != NULL)
 		return (NULL);
 
-	if (old_size > new_size)
-		return (NULL);
-
 	ptr1 = malloc(sizeof(char) * new_size);
 	if (ptr1 == NULL)
 		return (NULL);
