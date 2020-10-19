5b4c673ba85462413ad4a37ffe84e877e9d9493f
Maddi Laizure <2331@holbertonschool.com>
2020-10-19 20:39:11 +0000

    Fixed if/else logic

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,11 +15,13 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		printf("Name: (nil)");
-	printf("Name: %s\n", d->name);
+	else
+		printf("Name: %s\n", d->name);
 
 	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)");
-	printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: %s\n", d->owner);
 }
