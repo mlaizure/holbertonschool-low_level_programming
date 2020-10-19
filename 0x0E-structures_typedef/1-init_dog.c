655ccf0c49412450bb0599b08c649a7a1b508d6e
Maddi Laizure <2331@holbertonschool.com>
2020-10-19 21:20:03 +0000

    Added age check

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,6 +13,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
-	d->age = age;
+	if (d->age < 0)
+		d->age = 0;
+	else
+		d->age = age;
 	d->owner = owner;
 }
