c8c7b5ca430a948bc67a0ab573694854da58f8a5
Maddi Laizure <2331@holbertonschool.com>
2020-10-19 21:30:21 +0000

    Added NULL logic

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initializes variable of type struct dog
@@ -12,10 +13,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	if (d->age < 0)
-		d->age = 0;
-	else
+	if (d != NULL)
+	{
+		d->name = name;
 		d->age = age;
-	d->owner = owner;
+		d->owner = owner;
+	}
 }
