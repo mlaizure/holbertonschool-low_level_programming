c5cc030fcb490c423ba0d9f8426f96639edb5d6e
Maddi Laizure <2331@holbertonschool.com>
2020-10-19 21:13:57 +0000

    Updated dog header and task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,43 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: char pointer to name of dog
+ * @age: age of dog (float type)
+ * @owner: char pointer to name of dog's owner
+ *
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int nameLen = 0, ownerLen = 0, i = 0;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	while (name[nameLen] != '\0')
+		++nameLen;
+
+	dog->name = malloc(sizeof(char) * nameLen);
+	if (dog->name == NULL)
+		return (NULL);
+	for (i = 0; i < nameLen; ++i)
+		dog->name[i] = name[i];
+
+	while (owner[ownerLen] != '\0')
+		++ownerLen;
+
+	dog->owner = malloc(sizeof(char) * ownerLen);
+	if (dog->owner == NULL)
+		return (NULL);
+	for (i = 0; i < ownerLen; ++i)
+		dog->owner[i] = owner[i];
+
+	dog->age = age;
+
+	return (dog);
+}
