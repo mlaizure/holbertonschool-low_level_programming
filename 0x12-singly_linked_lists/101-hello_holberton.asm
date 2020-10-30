0036369cd4a2b1072dbecb9f55e91e16d02fd1ef
mlaizure <maddi.laizure@gmail.com>
2020-10-30 16:47:45 +0000

    Added exit call

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,15 @@
 section .text
 	global	main
 	extern	printf
+	extern	exit
+
 main:
 	mov	rdi, fmt
 	mov	rsi, msg
 	mov	al, 0
 	call 	printf
 
-	ret
+	call	exit
 
 section .data
 fmt db		"%s", 10, 0
