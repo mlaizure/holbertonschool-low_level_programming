1e3063cbe36b6e0038a327af61abf87457e6f21c
mlaizure <maddi.laizure@gmail.com>
2020-11-04 06:23:20 +0000

    Added null check

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head)
 		return (NULL);
 
-	for (i = 0; i < index; ++i)
+	for (i = 0; temp && i < index; ++i)
 		temp = temp->next;
 
 	return (temp);
