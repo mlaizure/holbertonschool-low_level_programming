6a0a6bc993eee6dbc162d12102734fc6d8992e20
mlaizure <maddi.laizure@gmail.com>
2020-11-09 20:02:59 +0000

    Changed number of written bytes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,15 +12,14 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, rd;
-	unsigned int wrt;
+	int fd, rd, wrt;
 	char *buf;
+
+	if (!filename)
+		return (0);
 
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
-		return (0);
-
-	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
@@ -31,8 +30,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (rd < 0)
 		return (0);
 
-	wrt = write(STDOUT_FILENO, buf, letters);
-	if (wrt != letters)
+	wrt = write(STDOUT_FILENO, buf, rd);
+	if (wrt < 0)
 		return (0);
 
 	close(fd);
