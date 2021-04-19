9744121387026f92a623db0cd312dad84e9c40c3
mlaizure <maddi.laizure@gmail.com>
2021-04-19 07:32:03 +0000

    Added NULL guard

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,6 +13,9 @@ int binary_search(int *array, size_t size, int value)
 	size_t mid;
 	int left = 0, right = size - 1, i;
 
+	if (!array)
+		return (-1);
+
 	while (left <= right)
 	{
 		mid = (left + right) / 2;
