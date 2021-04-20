a007bff23ca422b33a641c03fe6503682ef62aa7
mlaizure <maddi.laizure@gmail.com>
2021-04-20 18:26:37 +0000

    Added print statement

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -36,6 +36,8 @@ int interpolation_search(int *array, size_t size, int value)
 			return (pos);
 	}
 	if (value == array[low])
+		printf("Value checked array[%d] = [%d]\n",
+		       (int)low, array[low]);
 		return (low);
 	printf("Value checked array[%d] is out of range\n", (int)pos);
 	return (-1);
