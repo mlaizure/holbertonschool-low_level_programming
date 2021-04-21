f1a2797b0585ca5717cef917b59f2ab87bf96f32
mlaizure <maddi.laizure@gmail.com>
2021-04-21 00:09:01 +0000

    Reverting

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -32,7 +32,7 @@ int binary_search(int *array, size_t size, int value)
 		if (array[mid] == value)
 			return (mid);
 		if (array[mid] < value)
-			left = mid;
+			left = mid + 1;
 		else
 			right = mid - 1;
 	}
